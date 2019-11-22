37a696d96e288c91f25e6e4e86e9f0ae35c8be71
davilarodrigo <54740494+davilarodrigo@users.noreply.github.com>
2019-11-22 20:56:12 -0300

    se creo el proyecto Client y se renombro el primer proyecto como Server, se comenzo a codificar la logica de conexion en el proyecto Server

---
+++ Server/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void TcpServerRun()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 5004);
+            tcpListener.Start();
+
+            while (true)
+            {//es un loop para que se puedan hacer varias conexiones, cuando una conexion termina empieza a buscar otra
+                TcpClient client = tcpListener.AcceptTcpClient();
+                Thread tcpHandlerThread = new Thread(new ParameterizedThreadStart(tcpHandler));
+                tcpHandlerThread.Start(client);
+            }
+
+        }
+
+        private void tcpHandler(object client)
+        {
+            TcpClient mCLient = (TcpClient)client;
+            NetworkStream stream = mCLient.GetStream();
+            while (true)
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread tcpServerRunThread = new Thread(new ThreadStart(TcpServerRun));
+
+            tcpServerRunThread.Start();
+        }
+    }
+}
